ea573d2540332419c623a4fddc2fc43ee3b8f297
shuvo2480th-design <shuvo2480th@gmail.com>
2026-07-08 01:13:53 +0600

    Update otp.py

--- otp.py
+++ otp.py
@@ -274,11 +274,11 @@ def build_message(masked_number, flag, short_code, service, lang):
     current_time = bd_time()
     return (
         f"┌────────────────────┐\n"
-        f"│ ✦ {masked_number} ✦   │\n"
+        f"│ ✦ {masked_number} ✦\n"
         f"├────────────────────┤\n"
-        f"│ {flag} {short_code} • 👉 {service}│\n"
+        f"│ {flag} {short_code} • 👉 {service}\n"
         f"├────────────────────┤\n"
-        f"│ ⏰ {current_time} • #{lang} │\n"
+        f"│ ⏰ {current_time} • #{lang}\n"
         f"└────────────────────┘"
     )
 
